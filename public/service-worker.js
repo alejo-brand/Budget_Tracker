f2e254f055a04549c43109fa53999019855f85b7
Carlos Brand <alejandrobrand8@gmail.com>
2021-02-27 13:10:51 -0800

    service worker paths fixed

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,15 @@
 const FILES_TO_CACHE = [
   "/",
-  "/index.html",
+  "./index.html",
   "/all.css",
-  "/style.css",
-  "/db.js",
-  "/index.js",
-  "/manifest.webmanifest",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
+  "./styles.css",
+  "./db.js",
+  "./index.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png",
   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
   "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
- 
- 
 
 ];
 
