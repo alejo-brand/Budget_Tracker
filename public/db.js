a868e5d8b49d1c4833f915abc0a1bcd1853d0a45
Carlos Brand <alejandrobrand8@gmail.com>
2021-02-27 12:19:48 -0800

    webmanifest and service-worker added

--- public/db.js
+++ public/db.js
@@ -3,7 +3,7 @@ let db;
 const request = indexedDB.open("budget", 1);
 request.onupgradeneeded = function(event){
   const db = event.target.result;
-  db.createObjectStore("pending",{autoIncrement:true});
+  db.createObjectStore("transaction",{autoIncrement:true});
 };
 
 request.onsuccess = function(event){
