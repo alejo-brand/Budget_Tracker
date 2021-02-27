044217d6931aab70eff4fada14900bab85855641
Carlos Brand <alejandrobrand8@gmail.com>
2021-02-26 19:21:22 -0800

    function checkDatabase added

--- public/db.js
+++ public/db.js
@@ -23,3 +23,28 @@ function saveRecord(record){
   store.add(record);
 };
 
+function checkDatabase(){
+  const transaction = db.transaction(["transaction"],"readwrite");
+  const store = transaction.objectStore("transaction");
+  const getAll = store.getAll();
+    
+   getAll.onsuccess = function(){
+     if(getAll.result.length > 0){
+       fetch("/api/transaction/bulk",{
+         method:"POST",
+         body:JSON.stringify(getAll,result),
+         headers:{
+           Accept: "application/json, text/plain, */*","Content-type": "application/json"
+         }
+       }).then(response =>response.json())
+       .then(()=>{
+         const transaction = db.transaction(["transaction"],"readwrite");
+         const store = transaction.objectStore("transaction");
+         store.clear();
+       });
+     };
+   };
+
+};
+
+window.addEventListener("online",checkDatabase);
